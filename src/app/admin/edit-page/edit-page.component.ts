3d8a1ab5e8f4ef87e1b2197ce6e3480ff2453974
IIM <iim@norse.co>
2020-06-18 13:42:55 +0300

    edit post

--- src/app/admin/edit-page/edit-page.component.ts
+++ src/app/admin/edit-page/edit-page.component.ts
@@ -1,15 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { PostsService } from 'src/app/shared/posts.service';
+import { switchMap } from 'rxjs/operators';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Post } from 'src/app/shared/interfaces';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-edit-page',
   templateUrl: './edit-page.component.html',
   styleUrls: ['./edit-page.component.scss']
 })
-export class EditPageComponent implements OnInit {
+export class EditPageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  form: FormGroup
+  post: Post
+  submitted = false
+
+  updateSubscription: Subscription
+
+  constructor(
+    private route: ActivatedRoute,
+    private postsService: PostsService
+  ) { }
 
   ngOnInit(): void {
+    this.route.params.pipe(switchMap((params: Params) => {
+      return this.postsService.getById(params['id'])
+    })).subscribe((post: Post) => {
+      this.post = post
+      this.form = new FormGroup({
+        title: new FormControl(post.title, Validators.required),
+        text: new FormControl(post.text, Validators.required)
+      })
+    })
+  }
+
+  ngOnDestroy() {
+    if(this.updateSubscription) {
+      this.updateSubscription.unsubscribe()
+    }
+  }
+
+  submit() {
+    if(this.form.invalid) {
+      return
+    }
+    this.submitted = true
+
+    this.updateSubscription = this.postsService.update({
+      ...this.post,
+      text: this.form.value.text,
+      title: this.form.value.title,
+    }).subscribe(() => {
+      this.submitted = false
+    })
   }
 
 }
--- src/app/shared/posts.service.ts
+++ src/app/shared/posts.service.ts
@@ -36,4 +36,19 @@ export class PostsService {
   remove(id: string): Observable<void> {
     return this.http.delete<void>(`${environment.firebaseDatabaseUrl}/posts/${id}.json`)
   }
+
+  getById(id: string): Observable<Post> {
+    return this.http.get<Post>(`${environment.firebaseDatabaseUrl}/posts/${id}.json`)
+      .pipe(map((post: Post) => {
+        return {
+          ...post,
+          id,
+          date: new Date(post.date)
+        }
+      }))
+  }
+
+  update(post: Post): Observable<Post> {
+    return this.http.patch<Post>(`${environment.firebaseDatabaseUrl}/posts/${post.id}.json`, post)
+  }
 }
