be11ecf064ee756350c964d191208f0dd900ff2c
IIM <iim@norse.co>
2020-06-18 12:53:18 +0300

    dashboard

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { PostsService } from 'src/app/shared/posts.service';
+import { Post } from 'src/app/shared/interfaces';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard-page',
   templateUrl: './dashboard-page.component.html',
   styleUrls: ['./dashboard-page.component.scss']
 })
-export class DashboardPageComponent implements OnInit {
+export class DashboardPageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  posts: Post[] = []
+  postsSubscription: Subscription
+  searchStr = ''
+
+  constructor(
+    private postsService: PostsService
+  ) { }
 
   ngOnInit(): void {
+    this.postsSubscription = this.postsService.getAll().subscribe(posts => {
+      this.posts = posts
+    })
+  }
+
+  ngOnDestroy(): void {
+    if(this.postsSubscription) {
+      this.postsSubscription.unsubscribe()
+    }
+  }
+
+  remove(id: string) {
+
   }
 
 }
